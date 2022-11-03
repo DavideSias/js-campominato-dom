7e15174691bda338499b419bcd8f0b71d9155d30
DavideSias <davidesias77@gmail.com>
2022-11-03 16:07:44 +0100

    Add bomb game script and refactor style

--- js/script.js
+++ js/script.js
@@ -5,52 +5,54 @@ elePlay.addEventListener('click', function(){
     const eleGrid = document.querySelector('.grid');
     eleGrid.replaceChildren();  
     // eleGrid.innerHTML = '' alternativa per svuotare il contenuto
-    const eleDifficulty = document.querySelector('#difficulty').value;
+    const eleDifficulty = document.querySelector('#difficulty');
+    let nCells = parseInt(eleDifficulty.value);
+    console.log(nCells);
+    const arrRandomNumbers = [];
 
+    for(let i = 0; i < 16; i++){
+        let numberRandom;
+        do {
+            numberRandom = getRandomInteger(1, nCells);
+        } while (arrRandomNumbers.includes(numberRandom));
+        arrRandomNumbers.push(numberRandom);
+    }
+    console.log(arrRandomNumbers);
     
-    if (eleDifficulty === 'easy'){
-        for (let i = 1; i <= 100; i++) {
-            eleCell = document.createElement('div');
-            eleCell.classList.add('cell');
-            eleGrid.append(eleCell);
-            eleCell.innerHTML = i; 
-            eleCell.style.width = `calc(100% / 10)`;
-            eleCell.style.style = `calc(100% / 10)`;  
-            
-            eleCell.addEventListener('click', function() {
-                this.classList.toggle('active');
-                console.log([i]);
-            });                
-        }    
-    } else if (eleDifficulty === 'medium'){
-        for (let i = 1; i <= 81; i++) {
+    for (let i = 1; i <= nCells; i++) {
+        eleCell = document.createElement('div');
+        eleCell.classList.add('cell');
         
-            eleCell = document.createElement('div');
-            eleCell.classList.add('cell');
-            eleGrid.append(eleCell);
-            eleCell.innerHTML = i;
-            eleCell.style.width = `calc(100% / 9)`;
-            eleCell.style.style = `calc(100% / 9)`; 
-        
-            eleCell.addEventListener('click', function() {
-                this.classList.toggle('active');
-                console.log([i]);
-            });                
-        }
-    } else if (eleDifficulty === 'hard'){
-        for (let i = 1; i <= 49; i++) {
-          
-            eleCell = document.createElement('div');
-            eleCell.classList.add('cell');
-            eleGrid.append(eleCell);
-            eleCell.innerHTML = i;
-            eleCell.style.width = `calc(100% / 7)`;
-            eleCell.style.style = `calc(100% / 7)`; 
-        
-            eleCell.addEventListener('click', function() {
-                this.classList.toggle('active');
-                console.log([i]);
-            });                
+        if (arrRandomNumbers.includes(i)){
+            eleCell.classList.add('bomb');
+        } 
+
+        eleGrid.append(eleCell);
+        eleCell.innerHTML = i; 
+
+        eleCell.addEventListener('click', function() {
+            this.classList.add('active');
+            console.log([i]);
+        });
+
+        switch (nCells) {
+            case 100:
+                eleCell.style.width = `calc(100% / 10)`;
+                eleCell.style.style = `calc(100% / 10)`; 
+                break;
+            case 81:
+                eleCell.style.width = `calc(100% / 9)`;
+                eleCell.style.style = `calc(100% / 9)`; 
+                break;
+            case 49:
+                eleCell.style.width = `calc(100% / 7)`;
+                eleCell.style.style = `calc(100% / 7)`; 
+                break;
         }
     }
-})
+});
+
+function getRandomInteger(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
